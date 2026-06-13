06c76c59e320a769b43850f3bb75c1f5b6d2ed51
Andrew Nicoll <anicoll01@gmail.com>
2026-06-13 19:13:34 +0930

    fix: allow CORS credentials on backend server

--- backend/cmd/budgetd/main.go
+++ backend/cmd/budgetd/main.go
@@ -96,6 +96,7 @@ func withCORS(next http.Handler, origin string) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
