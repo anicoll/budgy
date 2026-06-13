58bc4453346cb90e1359fcaca879fd1bd1de3105
Andrew Nicoll <anicoll01@gmail.com>
2026-06-13 10:55:13 +0930

    feat(migration): replace 0002 migration with updated init schema

--- backend/internal/storage/migrations/0001_init.sql
+++ backend/internal/storage/migrations/0001_init.sql
@@ -10,30 +10,37 @@ CREATE TABLE budgets (
 
 CREATE TABLE accounts (
     id TEXT PRIMARY KEY,
-    budget_id TEXT NOT NULL,
+    budget_id TEXT,
     name TEXT NOT NULL,
     type TEXT NOT NULL,
     balance BIGINT NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    FOREIGN KEY(budget_id) REFERENCES budgets(id) ON DELETE CASCADE
+    class TEXT,
+    account_no TEXT,
+    available_funds BIGINT,
+    product TEXT,
+    institution_id TEXT,
+    connection_id TEXT,
+    last_updated TIMESTAMP,
+    FOREIGN KEY(budget_id) REFERENCES budgets(id) ON DELETE SET NULL
 );
 
 CREATE TABLE categories (
     id TEXT PRIMARY KEY,
-    budget_id TEXT NOT NULL,
+    budget_id TEXT,
     name TEXT NOT NULL,
     budgeted BIGINT NOT NULL DEFAULT 0,
     balance BIGINT NOT NULL DEFAULT 0,
     target_limit BIGINT NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    FOREIGN KEY(budget_id) REFERENCES budgets(id) ON DELETE CASCADE
+    FOREIGN KEY(budget_id) REFERENCES budgets(id) ON DELETE SET NULL
 );
 
 CREATE TABLE transactions (
     id TEXT PRIMARY KEY,
-    budget_id TEXT NOT NULL,
+    budget_id TEXT,
     account_id TEXT NOT NULL,
     category_id TEXT,
     amount BIGINT NOT NULL,
@@ -41,7 +48,14 @@ CREATE TABLE transactions (
     date TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    FOREIGN KEY(budget_id) REFERENCES budgets(id) ON DELETE CASCADE,
+    direction TEXT,
+    status TEXT,
+    class TEXT,
+    post_date TIMESTAMP,
+    sub_class TEXT,
+    raw_description TEXT,
+    merchant_name TEXT,
+    FOREIGN KEY(budget_id) REFERENCES budgets(id) ON DELETE SET NULL,
     FOREIGN KEY(account_id) REFERENCES accounts(id) ON DELETE CASCADE,
     FOREIGN KEY(category_id) REFERENCES categories(id) ON DELETE SET NULL
 );
@@ -51,3 +65,4 @@ DROP TABLE transactions;
 DROP TABLE categories;
 DROP TABLE accounts;
 DROP TABLE budgets;
+
